247935b14abb5ce719545f468a31408b365d7662
Ziirkov <55087514+Ziirkov@users.noreply.github.com>
2022-04-14 22:59:34 +0200

    Todo

---
+++ tp1cd/src/components/TodosForm.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="todos-form">
+        <input v-model="todoName" type="text" placeholder="Name of the todo"/>
+        <input v-model="todoTime" type="number" :min="0" :max="10" placeholder="Temps d'estimation"/>
+        <select v-model="todoPerson" >
+            <option disabled value="">Responsable</option>
+            <option>John</option>
+            <option>Doe</option>
+            <option>Foo</option>
+        </select>
+        <input type="submit" value="Bouton" @click="addTodo()" />
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'TodosForm',
+
+  props:{},
+  data: () => ({
+      errorMessage: '',
+      todoName: '',
+      todoTime: '',
+      todoPerson: '',
+      todos: []
+  }),
+  methods: {
+    addTodo() {
+      if(this.todoName.length === 0 || this.todoTime.length === 0 || this.todoPerson.length === 0  )
+      {
+        this.errorMessage = 'Veuillez remplir tous les champs !'
+      }else {
+          this.todos.push({
+            person: this.todoPerson,
+            name: this.todoName,
+            time: this.todoTime,
+            status: 'A faire'
+        })
+        this.errorMessage = ''
+      }
+    },
+  },
+}
+</script>
---
+++ tp1cd/src/views/TodosView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="todos">
+        <h1>Ma liste de Todos</h1>
+        <TodosForm />
+    </div>
+</template>
+
+<script>
+
+import TodosForm from '@/components/TodosForm.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    TodosForm,
+  },
+};
+
+</script>
